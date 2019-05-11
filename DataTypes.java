766c8eb3e5eb36efd3b0918a612c8d3b325486ce
Rakesh Kumar Singh <rksingh2004@gmail.com>
2019-05-12 01:46:49 +0530

    DataTypes.java
    examples of data types in Java

---
+++ DataTypes.java
@@ -0,0 +1,42 @@
+//Playing with datatypes
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Data types in Java
+		//Primitive datatype
+		int i = 1234512;
+		float f = 123232222222222222222222222222222.45f;
+		long l = 1223123123l;
+		double d = 12213123.12312312312312;
+		char c = 'R';
+		String s = "Rakesh";
+		boolean b = true; 
+		//referenced datatype
+		DataTypes dt = new DataTypes();
+		
+		//print data types variables
+		System.out.println("Value of int i is: "  +i);
+		System.out.println("Value of float f is: " +f );
+		System.out.println("Value of long l is: " +l);
+		System.out.println("Value of double d is: " +d);
+		System.out.println("Value of char c is " +c);
+		System.out.println("Value of string s is: "+s +" Singh");
+		System.out.println("Value of boolean b is: " +b);
+		System.out.println("Value of dt is: " +dt);
+		
+		//String concatenation
+		String s1 = "Rakesh ";
+		String s2 = "Nimisha ";
+		String s3 = "Ishani";
+		System.out.println(s1+s2+s3);
+		String s4 = s1+s2+s3;
+		System.out.println(s4);
+		
+		//calling methods from class - Methods
+		Methods m = new Methods();
+		m.
+		
+
+	}
+
+}

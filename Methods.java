e2861fc40f97bc931ef5ac8b23261acb93a11533
Rakesh Kumar Singh <rksingh2004@gmail.com>
2019-05-12 01:47:27 +0530

    Methods.java
    examples of methods in java

---
+++ Methods.java
@@ -0,0 +1,20 @@
+//Playing with methods
+public class Methods {
+			//Global variable
+			int i = 10;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Local variable
+		int j = 100;
+		//can access local variable and print
+		System.out.println(j);
+		//can't access global variable and print 
+		//System.out.println(i);
+		//unless initialise it locally
+		int i = 12;
+		System.out.println(i);
+			
+	}
+
+}
